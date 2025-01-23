9a43ed67110d873cdec652d8d2411f4b26b5a399
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-23 18:10:59 +0300

    complete the program logic and fix some bugs

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import string
 import random
-# ask user for the password length and the length of every content
 
-letters_len=0
-numbers_len=0
-symbols_len=0
+
 password_len = int(input("Enter the length of the password: "))
-# check for the password length against the sum of the user inputs:
-while password_len != (letters_len + numbers_len + symbols_len):
 
-    letters_len = int(input("Enter the number of letters: "))
-    numbers_len = int(input("Enter the number of numbers: "))
-    symbols_len = int(input("Enter the number of symbols: "))
-    print("Invalid inputs, the sum of the letters,numbers and symbol don't match the password length")
+# check for the password length against the sum of the user inputs:
+while True:
+
+    # ask user for the length of password contents
+    letters_len = input("Enter the number of letters: ")
+    numbers_len = input("Enter the number of numbers: ")
+    symbols_len = input("Enter the number of symbols: ")
+
+    if not letters_len.isdigit() or not numbers_len.isdigit() or not symbols_len.isdigit():
+        print("Invalid input, numbers only are acceptable.")
+
+    elif password_len == ( int(letters_len) + int(numbers_len) + int(symbols_len) ):
+        break
+
+    else:
+        print("Invalid inputs, the sum of the letters,numbers and symbol don't match the password length")
+
+letters= random.choices(string.ascii_letters, k=int(letters_len))
+numbers= random.choices(string.digits, k=int(numbers_len))
+symbols= random.choices(string.punctuation, k=int(symbols_len))
+
+password_as_list= letters + numbers + symbols
+
+random.shuffle(password_as_list)
+
+your_password= "".join(password_as_list)
+
+print(f"\nYour new password is: {your_password}")
 
 
 
